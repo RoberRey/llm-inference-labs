b34031457893147b925a2693bb337faf840444bc
Roberto Rey <robertoreysieiro@gmail.com>
2025-11-16 13:00:51 +0100

    included serving vllm bash script

---
+++ 1_baseline/scripts/serve_vllm.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+MODEL=${MODEL:-"Qwen/Qwen3-0.6B"}
+DTYPE=${DTYPE:-"float16"}
+GPU_UTIL=${GPU_UTIL:-"0.8"}
+MAX_LEN=${MAX_LEN:-"10240"}
+HOST=${HOST:-"0.0.0.0"}
+PORT=${PORT:-"8000"}
+
+echo "Starting vLLM locally with model: $MODEL"
+
+python -m vllm.entrypoints.openai.api_server \
+  --model "$MODEL" \
+  --dtype "$DTYPE" \
+  --gpu_memory_utilization "$GPU_UTIL" \
+  --max_model_len "$MAX_LEN" \
+  --host "$HOST" \
+  --port "$PORT"
